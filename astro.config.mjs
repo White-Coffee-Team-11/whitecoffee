bbf72c43929a77ac62e12eab08f183c56d68304e
Henrik von Oettingen <heoe0001@stud.kea.dk>
2025-05-29 13:49:22 +0200

    ved at vaere der

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
+
+export default defineConfig({
+  adapter: netlify(),
+});
